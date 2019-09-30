21b45f3b7dea08163d38f81b31ea43306a39bdb9
nkiyo <nkiyo@hoge.com>
2019-10-01 07:31:29 +0900

    up

--- apiserver.js
+++ apiserver.js
@@ -1,7 +1,18 @@
-const express = require('express')
-const app = express()
-const port = 8080
+const express = require("express");
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}`))
+const app = express();
+const port = 8080;
 
+app.get("/", (req, res) => {
+  console.log(`get /`);
+  res.send("Hello World!");
+});
+
+app.get("/hoge", (req, res) => {
+  console.log(`get /hoge`);
+  res.send("HOGE");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
